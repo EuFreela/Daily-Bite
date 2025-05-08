dbf1d3ec1c3eb02537e7934d8f7eae5d12433efc
Lameck Fernandes <lameck.fernandes@omni-electronica.com>
2025-05-08 00:30:42 -0300

    fix: dependencias  nao utilizadas

--- src/pages/Developer.tsx
+++ src/pages/Developer.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
   GitHubLogoIcon,
